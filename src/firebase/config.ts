aad704d554e21736497393fd7c9327011c1a362b
Yana B <y.barysheva@gmail.com>
2025-10-06 23:21:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8803128970-79b78",
+  "appId": "1:967095864677:web:d7b2affdaced1524dee58c",
+  "apiKey": "AIzaSyCUY33LpqrNzJDitNBVVsjOj4WRG7GuVkw",
+  "authDomain": "studio-8803128970-79b78.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "967095864677"
+};
